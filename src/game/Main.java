aa32c169bfed6c2ddccaec36cf2ec34b9890c57c
Jan Langela Regincos <jlangela@student.tgm.ac.at>
2019-12-13 15:15:48 +0100

    Startingpoint for the JavaFX App

---
+++ src/game/Main.java
@@ -0,0 +1,29 @@
+package game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    Controller c;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        this.c = loader.getController();
+        primaryStage.setTitle("PuyoPuyo");
+        Scene scene = new Scene(root, 300, 550);
+        primaryStage.setScene(scene);
+        scene.setOnKeyPressed(c);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
